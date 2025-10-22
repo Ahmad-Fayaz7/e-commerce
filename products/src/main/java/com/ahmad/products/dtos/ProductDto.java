6affe371008f39c3da21460522395d03fe186878
Ahmad Fayaz <fayaz.fazil76@gmail.com>
2025-10-22 08:39:50 +0200

    products-rest-api: Add unit testing for product service

--- products/src/main/java/com/ahmad/products/dtos/ProductDto.java
+++ products/src/main/java/com/ahmad/products/dtos/ProductDto.java
@@ -7,10 +7,16 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 @Schema(
         name = "Product",
         description = "Schema to hold product info"
