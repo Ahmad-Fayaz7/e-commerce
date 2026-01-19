8a4e9b5edfcc12e1ad906a59eb6fc6206afa2476
Ahmad Fayaz <fayaz.fazil76@gmail.com>
2026-01-19 17:00:39 +0100

    cart-rest-api: Remove  CartItemRepository and CartItemRepositoryTest.

--- carts/src/main/java/com/ahmad/carts/repositories/ICartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ahmad.carts.repositories;
-
-import com.ahmad.carts.entities.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICartItemRepository extends JpaRepository<CartItem, Long> {
-}
