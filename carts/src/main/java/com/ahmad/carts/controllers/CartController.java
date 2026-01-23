0d1cf018da7155c52f2cc06c547d4114ebcf42b8
Ahmad Fayaz <fayaz.fazil76@gmail.com>
2026-01-23 17:59:10 +0100

    cart-rest-api: Add user input validtaion.

--- carts/src/main/java/com/ahmad/carts/controllers/CartController.java
+++ carts/src/main/java/com/ahmad/carts/controllers/CartController.java
@@ -3,18 +3,22 @@ package com.ahmad.carts.controllers;
 import com.ahmad.carts.dtos.CartDto;
 import com.ahmad.carts.dtos.CartItemDto;
 import com.ahmad.carts.services.ICartService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = {"/api/carts"}, produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
 public class CartController {
     private final ICartService cartService;
     @PostMapping("/{customerId}/items")
-    ResponseEntity<CartDto> addItemToCart(@PathVariable Long customerId, @RequestBody  CartItemDto cartItemDto) {
+    ResponseEntity<CartDto> addItemToCart(@PathVariable Long customerId, @Valid @RequestBody  CartItemDto cartItemDto) {
         var cartDto = cartService.addItemToCart(customerId, cartItemDto);
         return ResponseEntity.status(HttpStatus.OK).body(cartDto);
     }
@@ -26,8 +30,8 @@ public class CartController {
     }
 
     @DeleteMapping("/{cartId}/items/{productId}")
-    ResponseEntity<CartDto> removeItemFromCart(@PathVariable String cartId, @PathVariable  String productId) {
-    var cartDto = cartService.removeItemFromCart(Long.valueOf(cartId), Long.valueOf(productId));
+    ResponseEntity<CartDto> removeItemFromCart(@PathVariable Long cartId, @PathVariable Long productId) {
+    var cartDto = cartService.removeItemFromCart(cartId, productId);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).body(cartDto);
     }
 
@@ -38,7 +42,7 @@ public class CartController {
     }
 
     @PutMapping("/{cartId}/items/{productId}/{quantity}")
-    ResponseEntity<CartDto> updateQuantity(@PathVariable Long cartId, @PathVariable Long productId, @PathVariable int quantity) {
+    ResponseEntity<CartDto> updateQuantity(@PathVariable Long cartId, @PathVariable Long productId, @Max(value = 100, message = "quantity must be less than or equal to 100") @PathVariable int quantity) {
         var cartDto = cartService.updateItemQuantity(cartId, productId, quantity);
         return ResponseEntity.status(HttpStatus.OK).body(cartDto);
     }
--- carts/src/main/java/com/ahmad/carts/dtos/CartItemDto.java
+++ carts/src/main/java/com/ahmad/carts/dtos/CartItemDto.java
@@ -1,5 +1,9 @@
 package com.ahmad.carts.dtos;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,7 +15,13 @@ import java.math.BigDecimal;
 @Builder
 
 public class CartItemDto {
+    @NotNull
     private Long productId;
+    @Min(1)
+    @Max(100)
     private int quantity;
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.00", inclusive = true,
+            message = "Price must be zero or positive")
     private BigDecimal price;
 }
