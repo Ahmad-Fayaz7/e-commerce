6e13b3019ecfa9ace5091ca55b344961c54d88a1
Ahmad Fayaz <fayaz.fazil76@gmail.com>
2026-01-25 20:09:46 +0100

    cart-rest-api: Optimize Cart tests.

--- carts/src/test/java/com/ahmad/carts/entities/CartTest.java
+++ carts/src/test/java/com/ahmad/carts/entities/CartTest.java
@@ -1,6 +1,7 @@
 package com.ahmad.carts.entities;
 
 import com.ahmad.carts.entities.enums.Currency;
+import com.ahmad.carts.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,9 +19,26 @@ class CartTest {
         cart = createCart();
         cartItem = createCartItem();
     }
+    @Test
+    void addItemToCart_shouldAddItemToCart() {
+        // sanity check
+        assertEquals(0, cart.getTotalItems());
+        assertEquals(BigDecimal.valueOf(0), cart.getTotalPrice());
+
+        // when
+        cart.addItemToCart(cartItem);
+
+        // then
+        assertFalse(cart.getItems().isEmpty());
+        assertTrue(cart.getItems().contains(cartItem));
+        assertEquals(cartItem.getCart(), cart);
+        assertEquals(2, cart.getTotalItems());
+        assertEquals(BigDecimal.valueOf(20), cart.getTotalPrice());
+    }
+
 
     @Test
-    void calculateTotals() {
+    void calculateTotals_shouldCalculateTotalPriceAndTotalItems() {
         // given
         cart.addItemToCart(cartItem);
 
@@ -33,20 +51,44 @@ class CartTest {
     }
 
     @Test
-    void shouldGetItemByProductId() {
+    void calculateTotals_emptyCart_shouldReturnZeroTotals() {
+        Cart cart = new Cart();
+
+        cart.calculateTotals();
+
+        assertEquals(0, cart.getTotalItems());
+        assertEquals(BigDecimal.ZERO, cart.getTotalPrice());
+    }
+
+
+    @Test
+    void getItemByProductId_whenItemExists_shouldReturnItem() {
         // given
         cart.addItemToCart(cartItem);
 
         // when
-        var savedItem = cart.getItemByProductId(cartItem.getProductId());
+        var result = cart.getItemByProductId(cartItem.getProductId());
 
         // then
-        assertTrue(savedItem.isPresent());
+        assertTrue(result.isPresent());
+        assertEquals(3L, result.get().getProductId());
+    }
+
+    @Test
+    void getItemByProductId_whenItemDoesNotExists_shouldReturnEmpty() {
+        // given
+        cart.addItemToCart(cartItem);
+
+        // when
+        var result = cart.getItemByProductId(99L);
+
+        // then
+        assertTrue(result.isEmpty());
 
     }
 
     @Test
-    void shouldRemoveItemByProductId() {
+    void removeItemByProductId_whenItemExists_shouldRemoveItem() {
         // given
         cart.addItemToCart(cartItem);
         assertTrue(cart.getItemByProductId(cartItem.getProductId()).isPresent());
@@ -61,6 +103,56 @@ class CartTest {
         assertEquals(0, cart.getTotalItems());
         assertEquals(BigDecimal.valueOf(0), cart.getTotalPrice());
     }
+
+    @Test
+    void removeItemByProductId_whenItemDoesNotExists_shouldThrowException() {
+        // when + then
+        ResourceNotFoundException exception = assertThrows(
+                ResourceNotFoundException.class,
+                () -> cart.removeItemByProductId(99L)
+        );
+
+        // then
+        assertEquals(
+                "CartItem with productId: 99 not found.",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    void updateQuantity_whenValid_shouldUpdateItemAndRecalculateTotals() {
+        // given
+        cart.addItemToCart(cartItem);
+
+        // sanity check
+        assertEquals(2, cart.getTotalItems());
+        assertEquals(BigDecimal.valueOf(20), cart.getTotalPrice());
+
+        // when
+        cart.updateQuantity(3L, 5);
+
+        // then
+        assertEquals(5, cartItem.getQuantity());
+        assertEquals(5, cart.getTotalItems());
+        assertEquals(BigDecimal.valueOf(50), cart.getTotalPrice());
+    }
+
+    @Test
+    void updateQuantity_whenQuantityLessThanOne_shouldThrowIllegalArgumentException() {
+        // given
+        cart.addItemToCart(cartItem);
+
+        // sanity check
+        assertEquals(2, cart.getTotalItems());
+        assertEquals(BigDecimal.valueOf(20), cart.getTotalPrice());
+
+        // when
+        var exception = assertThrows(IllegalArgumentException.class, () -> cart.updateQuantity(3L, -5));
+
+        // then
+        assertEquals("Quantity cannot be less than 1", exception.getMessage());
+    }
+
 
     private CartItem createCartItem() {
         return CartItem.builder().productId(3L).quantity(2).price(BigDecimal.valueOf(10)).build();
